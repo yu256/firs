c37ad6e97b292310fd13d800be41ae4d5a8c8b57
yu256 <122450013+yu256@users.noreply.github.com>
2024-10-04 21:29:52 +0900

    Parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main (main) where
 
-import Lib
+import Parser
+import Text.Megaparsec.Error
 
 main :: IO ()
-main = hello
+main = do
+  input <- getLine
+  case parseProgram input of
+    Left err -> putStr $ errorBundlePretty err
+    Right exprs -> print exprs
---
+++ src/Parser.hs
@@ -0,0 +1,112 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Parser (parseProgram) where
+
+import Control.Monad.Combinators.Expr
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+data Expr
+  = IntLit Integer
+  | StringLit String
+  | Var String
+  | ValDeclare String String Expr -- Value Name, Type Name, Value
+  | VarDeclare String String Expr -- Variable Name, Type Name, Value
+  | Assign String Expr
+  | BinaryOp String Expr Expr
+  | ArrayLit [Expr]
+  | IfExpr Expr Expr (Maybe Expr)
+  | FuncDeclare String [(String, String)] String Expr -- Function Name, [(ArgName, Type)], ReturnType, Body
+  deriving (Show, Eq)
+
+type Parser = Parsec Void String
+
+sc :: Parser ()
+sc = L.space space1 (L.skipLineComment "//") (L.skipBlockComment "/*" "*/")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+identifier :: Parser String
+identifier = lexeme $ (:) <$> letterChar <*> many alphaNumChar
+
+typeAnnotation :: Parser String
+typeAnnotation = symbol ":" *> identifier
+
+pIntLit :: Parser Expr
+pIntLit = IntLit <$> lexeme L.decimal
+
+pStringLit :: Parser Expr
+pStringLit = StringLit <$> lexeme (char '"' *> manyTill L.charLiteral (char '"'))
+
+pArrayLit :: Parser Expr
+pArrayLit = ArrayLit <$> (symbol "[" *> pExpr `sepBy` symbol "," <* symbol "]")
+
+pVar :: Parser Expr
+pVar = Var <$> identifier
+
+pValDeclare :: Parser Expr
+pValDeclare = ValDeclare <$> identifier <*> typeAnnotation <* symbol "=" <*> pExpr
+
+pVarDeclare :: Parser Expr
+pVarDeclare = VarDeclare <$> identifier <*> typeAnnotation <* symbol ":=" <*> pExpr
+
+pAssign :: Parser Expr
+pAssign = Assign <$> identifier <* symbol "<-" <*> pExpr
+
+pIfExpr :: Parser Expr
+pIfExpr =
+  let cond = symbol "if" *> pExpr <* symbol "then"
+      elseExpr = optional $ symbol "else" *> pExpr
+   in IfExpr <$> cond <*> pExpr <*> elseExpr
+
+pFuncDeclare :: Parser Expr
+pFuncDeclare =
+  let arg = (,) <$> identifier <*> typeAnnotation
+      argAndTypes = symbol "(" *> (arg `sepBy` symbol ",") <* symbol ")"
+      body = symbol "=" *> pExpr
+   in FuncDeclare <$> identifier <*> argAndTypes <*> typeAnnotation <*> body
+
+pBinaryOp :: Parser Expr
+pBinaryOp = makeExprParser pTerm operatorTable
+  where
+    operatorTable =
+      [ [InfixL $ BinaryOp "*" <$ symbol "*", InfixL $ BinaryOp "/" <$ symbol "/"],
+        [InfixL $ BinaryOp "+" <$ symbol "+", InfixL $ BinaryOp "-" <$ symbol "-"]
+      ]
+
+pExpr :: Parser Expr
+pExpr =
+  choice
+    [ pIfExpr,
+      try pValDeclare,
+      try pVarDeclare,
+      try pAssign,
+      try pFuncDeclare,
+      pBinaryOp,
+      pArrayLit,
+      pIntLit,
+      pStringLit,
+      pVar
+    ]
+
+pTerm :: Parser Expr
+pTerm =
+  choice
+    [ pIntLit,
+      pStringLit,
+      pVar,
+      pArrayLit,
+      symbol "(" *> pExpr <* symbol ")"
+    ]
+
+pProgram :: Parser [Expr]
+pProgram = many pExpr
+
+parseProgram :: String -> Either (ParseErrorBundle String Void) [Expr]
+parseProgram = parse pProgram ""
