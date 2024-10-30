b67dd70faa31d9e2ba4cf9ea0a33df40d392cde9
yu256 <122450013+yu256@users.noreply.github.com>
2024-10-30 20:51:43 +0900

    デバッグ時にASTを出力するように

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,8 @@
 module Main (main) where
 
 import Codegen (printLLVM)
-import Data.Maybe (fromMaybe, listToMaybe)
+import Control.Monad (when)
+import Data.Maybe (listToMaybe)
 import qualified Data.Text.Lazy.IO as TLIO
 import Parser
 import System.Environment (getArgs)
@@ -13,14 +14,24 @@ import Text.Megaparsec.Error
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    fileName : rest -> do
-      let outputPath = rest `headOr` "a.ll"
-      input <- readFile fileName
+  let (fileName, debugFlag, outputPath) = parseArgs args
+  case fileName of
+    Just fName -> do
+      input <- readFile fName
       case parseProgram input of
         Left err -> ePutStrLn $ errorBundlePretty err
-        Right (externs, ast) -> TLIO.writeFile outputPath $ printLLVM externs ast
-    _ -> ePutStrLn "Usage: <program> <source-file> <output>"
+        Right (externs, ast) -> do
+          when debugFlag $ print ast
+          TLIO.writeFile outputPath $ printLLVM externs ast
+    Nothing -> ePutStrLn "Usage: <program> <source-file> [-o <output>] [--debug]"
   where
-    headOr = flip fromMaybe . listToMaybe
     ePutStrLn = hPutStrLn stderr
+
+parseArgs :: [String] -> (Maybe String, Bool, String)
+parseArgs args =
+  let fileName = listToMaybe [arg | arg <- args, head arg /= '-']
+      debugFlag = "--debug" `elem` args
+      outputPath = case dropWhile (/= "-o") args of
+        ("-o" : out : _) -> out
+        _ -> "a.ll"
+   in (fileName, debugFlag, outputPath)
